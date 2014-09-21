2d6be931a832c9092b30e7bbc67723243e4d545b
alfred sang <i5ting@126.com>
2014-09-21 21:43:07 +0800

    init

--- d/routes/index.js
+++ d/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var Handlebars = require('handlebars');
+
+var BufferHelper = require('bufferhelper');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,11 +16,28 @@ router.get('/', function(req, res) {
 	
 	 var fs = require('fs');
  	 
-	 var ws = fs.createWriteStream(path.join('public/dataStream.json'), { encoding: "utf8" })
+	 var ws = fs.createWriteStream('public/dataStream.json', { encoding: "utf8" })
 
 	 ws.write(o); 
 	 ws.end(); // 目前和destroy()和destroySoon()一样 
 	 
+	 
+	var source = bufferHelper.toBuffer().toString();
+	var template = Handlebars.compile(source);
+	
+	
+	var rs = fs.createReadStream('res/template.html', {encoding: 'utf-8', bufferSize: 11}); 
+	var bufferHelper = new BufferHelper();
+
+	rs.on("data", function (trunk){
+			bufferHelper.concat(trunk);
+	});
+
+	rs.on("end", function () {
+		var source = bufferHelper.toBuffer().toString();
+		var template = Handlebars.compile(source);
+
+	});
 	
 	 res.render('index', { title: 'Express' });
  
